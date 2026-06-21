2f5f213ec85c0a74e926ae0cb2afc0207affe6c5
AgustinOjeda21 <agustinojeda398@gmail.com>
2026-06-20 23:27:02 -0300

    arreglo de SP_ProductoCategoria

--- SP_ProductoCategoria.sql
+++ SP_ProductoCategoria.sql
@@ -1,7 +1,7 @@
 CREATE PROCEDURE SP_ProductoCategoria(@Categoria VARCHAR(50))
 AS 
 BEGIN
-SELECT p.idProducto,p.Nombre,p.Precio,p.Stock,m.Nombre,c.Nombre FROM Producto AS p
+SELECT p.idProducto,p.Nombre,p.Precio,p.Stock,m.Nombre AS "Marca",c.Nombre AS "Categoria" FROM Producto AS p
 INNER JOIN Marca AS m ON m.idMarca = p.Marca_idMarca
 INNER JOIN Categoria AS c ON c.idCategoria = p.Categoria_idCategoria
 WHERE @Categoria = c.Nombre
