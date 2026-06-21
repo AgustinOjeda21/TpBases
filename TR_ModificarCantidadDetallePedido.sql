b8bab59fcc7661c6994243b0776c1da33457851e
AgustinOjeda21 <agustinojeda398@gmail.com>
2026-06-20 23:53:49 -0300

    Remove total update from Pedido in stock modification
    Removed update of total in Pedido table during stock modification.

--- TR_ModificarCantidadDetallePedido.sql
+++ TR_ModificarCantidadDetallePedido.sql
@@ -17,10 +17,6 @@ UPDATE p SET P.Stock = P.Stock + (d.Cantidad - i.Cantidad)
 FROM Producto AS p
 INNER JOIN inserted AS i ON p.idProducto = i.Producto_idProducto
 INNER JOIN deleted AS d ON p.idProducto = d.Producto_idProducto
-UPDATE pe SET Total = Total + (d.Cantidad-i.Cantidad)*i.PrecioUnitario
-FROM Pedido AS pe   
-INNER JOIN deleted AS d ON pe.idPedido = d.Pedido_idPedido
-INNER JOIN inserted AS i ON pe.idPedido = i.Pedido_idPedido
 COMMIT TRANSACTION
 END TRY
 BEGIN CATCH
