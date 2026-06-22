2b833aab86f8378134d4ce236d573f2f4c59b31a
AgustinOjeda21 <agustinojeda398@gmail.com>
2026-06-22 16:17:44 -0300

    Replace THROW with RAISERROR for error handling

--- SP_AgregarProducto.sql
+++ SP_AgregarProducto.sql
@@ -4,28 +4,31 @@ BEGIN
 BEGIN TRY 
 IF(@Stock<=0)
 BEGIN
-THROW 50001, 'El stock no puede ser menor o igual a 0', 1
+RAISERROR('El stock no puede ser menor o igual a 0', 16, 1)
 RETURN
 END
 IF(@Precio<=0)
 BEGIN
-THROW 50001, 'El precio no puede ser menor o igual a 0', 1
+RAISERROR('El precio no puede ser menor o igual a 0', 16, 1)
 RETURN
 END
 IF NOT EXISTS(SELECT 1 FROM Marca WHERE IdMarca = @Marca)
 BEGIN
-THROW 50002, 'La Marca ingresada no existe', 1;
+RAISERROR('La marca ingresada no existe', 16, 1)
 RETURN
 END
 IF NOT EXISTS(SELECT 1 FROM Categoria WHERE idCategoria = @Categoria)
 BEGIN
-THROW 50003, 'La Categoria ingresada no existe', 1;
+RAISERROR('La categoria ingresada no existe', 16, 1)
 RETURN
 END
 INSERT INTO Producto (Nombre,Precio,Stock,Marca_idMarca,Categoria_idCategoria)
 VALUES(@Nombre,@Precio,@Stock,@Marca,@Categoria)
 END TRY
 BEGIN CATCH
-THROW
+DECLARE @Mensaje VARCHAR(500) = ERROR_MESSAGE()
+DECLARE @Severidad INT = ERROR_SEVERITY()
+DECLARE @Estado INT = ERROR_STATE()
+RAISERROR(@Mensaje, @Severidad, @Estado)
 END CATCH
 END
